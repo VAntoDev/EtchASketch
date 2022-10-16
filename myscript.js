a89e476cdabde78b2958e9b7cf80bc701b2b00e1
VAnto <vntantonio@gmail.com>
2022-10-16 11:32:25 +0200

    Add colorBackground()

--- myscript.js
+++ myscript.js
@@ -3,11 +3,25 @@ const gridContainer = document.querySelector("#grid-container")
 const square = document.createElement('span')
 square.setAttribute('class', 'square');
 
-for(i = 0; i < 16; i++){
+
+for(i = 0; i < 32; i++){
     const square = document.createElement('span')
     square.setAttribute('class', 'square'); 
     gridContainer.appendChild(square);
 }
+
+
+const squares = document.getElementsByClassName("square");
+
+for (let i = 0 ; i < squares.length; i++) {
+    squares[i].addEventListener("mouseover", colorBackground, false); 
+    function colorBackground()
+    {  
+        squares[i].setAttribute("style", "background-color:green;")
+    }
+ }
+
+
 
 
 
